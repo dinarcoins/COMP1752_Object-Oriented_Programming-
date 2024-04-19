bd8358ab353d168d5ece39571824355577bd25b9
Mac <mac@Macs-MacBook-Pro-2.local>
2024-04-19 10:59:08 +0700

    change: change constant file

--- constants.py
+++ constants.py
@@ -1,7 +1,10 @@
 # constants.py
 
 video_list = [
-    {"id": 1, "name": "Video 1", "director": "Director A", "rating": 4, "play_count": 100},
-    {"id": 2, "name": "Video 2", "director": "Director B", "rating": 3, "play_count": 200},
-    {"id": 3, "name": "Video 3", "director": "Director C", "rating": 5, "play_count": 150},
+    {"id": 1, "name": "Tom and Jerry", "director": "Fred Quimby", "rating": 4, "play_count": 100},
+    {"id": 2, "name": "Breakfast at Tiffany's", "director": "Blake Edwards", "rating": 3, "play_count": 200},
+    {"id": 3, "name": "Casablanca", "director": "Michael Curtiz", "rating": 5, "play_count": 150},
+    {"id": 4, "name": "The Sound of Music", "director": "Robert Wise", "rating": 5, "play_count": 150},
+    {"id": 5, "name": "Gone with the Wind", "director": "Victor Fleming ", "rating": 5, "play_count": 150},
+    {"id": 6, "name": "The Golden King", "director": "DinarCoins ", "rating": 5, "play_count": 150},
 ]
